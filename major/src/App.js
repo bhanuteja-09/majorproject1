ace48bab0a60c5364eef6d2d7496ea883471cae6
Bhanu Tejaswini Kaluva <bhanutejaswinikaluva@gmail.com>
2024-08-06 14:19:27 +0530

    adding

--- major/src/App.js
+++ major/src/App.js
@@ -1,29 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import Header from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import FindAJob from './pages/FindAJob';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import './App.css'; // Import global CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// const DebugLocation = () => {
-//   const location = useLocation();
-//   console.log('Current URL:', location.pathname);
-//   return null;
-// };
+// import Register from './components/Register';
+// import Login from './components/Login';
 
 const App = () => {
   return (
-    <Router basename="/">
-      <Header />
-      {/* <DebugLocation /> */}
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/find-a-job" element={<FindAJob />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        {/* <Route path="/register" element={<Register />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
       </Routes>
     </Router>
   );
